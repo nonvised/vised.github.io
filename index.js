99ab239183a06ac0d515b6bf5382ef523b3b29fb
nonvised <124870230+nonvised@users.noreply.github.com>
2023-03-19 19:32:35 +0100

    site

---
+++ index.js
@@ -0,0 +1,8 @@
+setTimeout(function(){
+    $("#loading").addClass("animated fadeOut");
+    setTimeout(function(){
+      $("#loading").removeClass("animated fadeOut");
+      $("#loading").css("display","none");
+      $("#box").css("display","none");
+    },1000);
+},1500);
